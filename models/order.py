5333773bf638d6aea72ec556a46e43ad5d318c4e
Vansh Saini <deeptisaini24@gmail.com>
2026-01-22 23:00:38 +0530

    optimized order class

--- models/order.py
+++ models/order.py
@@ -1,10 +1,10 @@
-from models.price import Price
 from dataclasses import dataclass
 
 @dataclass(slots=True)
 class Order:
-    order_id: int
-    side: str
-    price: Price
-    quantity: int
-    order_type: str
+    order_id: int       # unique identifier for the order
+    side: int           # 1 for buy, -1 for sell
+    price: int          # price in ticks
+    quantity: int       # quantity in lots
+    order_type: int     # 0 for limit order, 1 for market order, 2 for cancel order
+    timestamp: int      # time when the order was placed
